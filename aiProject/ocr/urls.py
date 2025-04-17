a2f0026e025e41efe7cfd04dc1fd02d13b29870b
PV25227 <rubenpintosousa@gmail.com>
2025-04-17 20:48:25 +0100

    Delete aiProject directory

--- aiProject/ocr/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .ocr import upload_image
-
-urlpatterns = [
-    path('upload/', upload_image, name='upload_image'),
-]
--- aiProject/web/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import interface
-
-
-
-urlpatterns = [
-    path('', interface, name='interface'),
-]
-
